51b362a454fa1114ba2ce8c0858ddc2389260e39
yeliz601 <yeliz601@BPC11>
2023-12-21 13:24:24 +0300

    Proje dosyası ekle.

---
+++ ArayuzlerSiraSizde/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArayuzlerSiraSizde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap kitap = new Kitap();
+            kitap.Oku();
+            Console.WriteLine("====================================");
+            Dergi dergi = new Dergi();
+            dergi.Oku();
+            Console.WriteLine("====================================");
+            Ansiklopedi ansiklopedi = new Ansiklopedi();
+            ansiklopedi.Oku();
+            Console.WriteLine("====================================");
+        }
+    }
+    interface IOrtak
+    {
+        void Oku();
+    }
+    public class Kitap : IOrtak
+    {
+        public void Oku()
+        {
+            Console.WriteLine("kitap okundu.");
+        }
+    }
+    public class Dergi : IOrtak
+    {
+        public void Oku()
+        {
+            Console.WriteLine("dergi okundu.");
+        }
+    }
+    public class Ansiklopedi : IOrtak
+    {
+        public void Oku()
+        {
+            Console.WriteLine("ansiklopedi okundu.");
+        }
+    }
+    
+}
